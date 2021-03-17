28acafd500bbe69313e6d76cd3876fd85894379f
Bogdan Negura <id.bogdann@yahoo.com>
2021-03-17 21:27:02 +0000

    add styled-components and create the header structure

---
+++ src/component/header/header.style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const StyledHeaderWrapper = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: #FF6B35;
+  display: flex;
+  justify-content: space-between;
+`
+
+export const StyledHeaderRight = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const StyledHeaderLeft = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
--- src/component/header/header.tsx
+++ src/component/header/header.tsx
@@ -1,9 +1,21 @@
 import React from "react"
+import * as HeaderST from "./header.style"
 
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <HeaderST.StyledHeaderWrapper>
+      <HeaderST.StyledHeaderLeft>
+        <div>Builderoo.<span>uk</span></div>
+        <div>Developments</div>
+      </HeaderST.StyledHeaderLeft>
+      <HeaderST.StyledHeaderRight>
+        <button>Prost a job</button>
+        <nav>
+          <div>Client</div>
+          <div>Trade member</div>
+          <div>Login</div>
+        </nav>
+      </HeaderST.StyledHeaderRight>
+    </HeaderST.StyledHeaderWrapper>
   )
 } 
